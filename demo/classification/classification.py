906f85ea6c55aeb0639f3655bf7b64040d74d9d0
weenit <dev.weenit@gmail.com>
2018-07-10 12:13:07 +0545

    Added scaling

--- demo/classification/classification.py
+++ demo/classification/classification.py
@@ -23,8 +23,21 @@ plt.show()
 data['Age'] = np.log(data['Age'])
 
 # Normalize Data using zscore
+zscore_scaler = preprocessing.Normalizer()
+data['PlasmaGlucose'] = zscore_scaler.fit_transform(data[['PlasmaGlucose']])
+data['DiastolicBloodPressure'] = zscore_scaler.fit_transform(data[['DiastolicBloodPressure']])
+data['BMI'] = zscore_scaler.fit_transform(data[['BMI']])
 
 # Normalize Data using min max
+minmax_scaler = preprocessing.MinMaxScaler()
+data['Pregnancies'] = minmax_scaler.fit_transform(data[['Pregnancies']])
+data['TricepsThickness'] = minmax_scaler.fit_transform(data[['TricepsThickness']])
+data['SerumInsulin'] = minmax_scaler.fit_transform(data[['SerumInsulin']])
+data['DiabetesPedigree'] = minmax_scaler.fit_transform(data[['DiabetesPedigree']])
+data['Age'] = minmax_scaler.fit_transform(data[['Age']])
+
+sns.pairplot(data)
+plt.show()
 
 # Edit metadata for training
 features = data
@@ -46,6 +59,11 @@ bdt.fit(X_train, y_train)
 
 # Predict
 predictions = bdt.predict(X_test)
+print(predictions[5:20])
+print(y_test[5:20])
+
 # Score Model
+print ('Score:', bdt.score(X_test, y_test))
 
 # Evaluate Model
+'''How to evaluate model'''
